ca357e2b96bab95b1a5cb94967e9f5e59de2c4fe
Kevin <89570292+Kevin28576@users.noreply.github.com>
2022-07-22 20:30:18 +0800

    初版

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require('..\\Documents\\GitHub\\Documentation\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('..\\Documents\\GitHub\\Documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('..\\Documents\\GitHub\\Documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('..\\Documents\\GitHub\\Documentation\\src\\css\\custom.css'),
+  require('C:\\Users\\戴梓恆\\Documents\\GitHub\\Documentation\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\戴梓恆\\Documents\\GitHub\\Documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\戴梓恆\\Documents\\GitHub\\Documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\戴梓恆\\Documents\\GitHub\\Documentation\\src\\css\\custom.css'),
 ];
